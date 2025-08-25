f965e123e1cfbec852d07b06b7370fbece398124
Rakandhiya Rachmanto <rakandhiya.daanii@gmail.com>
2025-08-25 00:58:43 -0400

    chore: restructuring src/sensors/deploy to src/sensing

--- src/sensors/deploy/tm1/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-timeout --preserve-status 30s digitemp_DS9097 -n 0 -d 1 -t 0 -q
--- src/sensors/deploy/tm2/run.sh
+++
@@ -1,9 +0,0 @@
-
-intf="/dev/ttyACM0"
-freq=9600
-
-$1 compile --fqbn arduino:avr:uno tm2.ino
-$1 upload -p $intf --fqbn arduino:avr:uno tm2.ino
-stty -F $intf $freq raw -clocal -echo
-cat $intf
-
